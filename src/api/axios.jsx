5438752cb6f80e74163838ec77e3768b4b4dd1b2
Muhammad-Mairaj <muhammadmairajj@gmail.com>
2023-04-14 23:05:56 +0500

    api add

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,5 +2,6 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:'http://localhost:8080/'
+    baseURL:"http://ec2-3-95-200-237.compute-1.amazonaws.com:8080"
+    // baseURL:'http://localhost:8080/'
 })
