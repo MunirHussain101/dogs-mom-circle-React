e93fef47b513fc91f0e754e94295b64fcc077411
Muhammad-Mairaj <muhammadmairajj@gmail.com>
2023-04-28 21:00:58 +0500

    build and url change

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: "http://ec2-44-206-231-202.compute-1.amazonaws.com:8080/"
-    // baseURL:"http://ec2-3-95-200-237.compute-1.amazonaws.com:8080/" 
+    baseURL: "http://ec2-3-93-146-137.compute-1.amazonaws.com:8080/"  
     // baseURL:'http://localhost:8080/'
     
 })
