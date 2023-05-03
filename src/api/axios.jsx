06be0178100196bab9556f7421d37447a54e3c4d
Muhammad-Mairaj <muhammadmairajj@gmail.com>
2023-05-03 21:46:38 +0500

    new link update

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: "http://ec2-3-93-146-137.compute-1.amazonaws.com:8080/"  
+    baseURL: "http://ec2-3-93-238-241.compute-1.amazonaws.com:8080/"
+    // baseURL: "http://ec2-3-93-146-137.compute-1.amazonaws.com"  
     // baseURL:'http://localhost:8080/'
     
 })
